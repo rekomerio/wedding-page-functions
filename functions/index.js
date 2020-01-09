3e84c42c97d94e4d337be442fe0ab01d6c3024c5
Reko Meriö <K9260@student.jamk.fi>
2020-01-09 23:02:51 +0200

    Refactor and change logging

--- functions/index.js
+++ functions/index.js
@@ -79,7 +79,6 @@ exports.removeGiftReservation = functions
     .https.onCall(async (data, context) => {
         const { uid } = context.auth;
         const { giftId } = data;
-        let message = "Ok";
 
         if (!uid) {
             return { message: "Unauthorized" };
@@ -94,50 +93,51 @@ exports.removeGiftReservation = functions
         const doc = await docRef.get();
 
         if (!doc.exists) {
-            message = "No such gift " + giftId;
-            console.log(message);
-        } else {
-            const gift = doc.data();
-            if (gift.reservedBy === uid) {
-                await docRef.update({ reservedBy: "" });
-            } else {
-                message = "Gift reserved by someone else " + gift.reservedBy;
-                console.log(message);
-            }
+            return { message: "Gift not found", giftId };
         }
 
-        return { message: message, gift: giftId };
+        const gift = doc.data();
+
+        if (gift.reservedBy === uid) {
+            // Remove reservation
+            await docRef.update({ reservedBy: "" });
+        } else {
+            console.log(uid, "attempted to unreserve gift reserved by", gift.reservedBy);
+            return { message: "You have not made the gift reservation", giftId };
+        }
+
+        console.log(giftId, "unreserved by", uid);
+        return { message: "Ok", giftId };
     });
 
 exports.reserveGift = functions.region("europe-west1").https.onCall(async (data, context) => {
     const { uid } = context.auth;
     const { giftId } = data;
-    let message = "Ok";
 
     if (!uid) {
         return { message: "Unauthorized" };
     }
 
     if (!giftId) {
-        return { message: "Gift id was: " + giftId };
+        return { message: "giftId is required", giftId };
     }
 
     const docRef = db.collection("gifts").doc(giftId);
-
     const doc = await docRef.get();
 
     if (!doc.exists) {
-        message = "No such gift " + giftId;
-        console.log(message);
-    } else {
-        const gift = doc.data();
-        if (gift.reservedBy) {
-            message = "Gift already reserved by " + gift.reservedBy;
-            console.log(message);
-        } else {
-            await docRef.update({ reservedBy: uid, reservedAt: Date.now() });
-        }
+        return { message: "Gift not found", giftId };
     }
 
-    return { message: message, gift: giftId };
+    const gift = doc.data();
+
+    if (gift.reservedBy) {
+        console.log(uid, "attempted to reserve gift reserved by", gift.reservedBy);
+        return { message: "Gift is already reserved", giftId };
+    }
+
+    await docRef.update({ reservedBy: uid, reservedAt: Date.now() });
+
+    console.log(giftId, "reserved to", uid);
+    return { message: "Ok", giftId };
 });
